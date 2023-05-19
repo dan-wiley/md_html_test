9c6bf17d5b4f1908ef2653bae632784257dd3dbf
dan-wiley <119693510+dan-wiley@users.noreply.github.com>
2023-05-19 14:33:01 -0700

    Update add-class.lua

--- add-class.lua
+++ add-class.lua
@@ -1,6 +1,22 @@
-function Div(el)
-  if el.classes[1] == 'my-tag-class' then
-    el.classes:insert(1, 'new-class')
+-- eg. Input
+-- <li><p>old</p></li>
+-- Output:
+-- <li>old</li>
+function removePTagsInLiBlocks (element)
+  if element.tag == "BulletList" or element.tag == "OrderedList" then
+    -- Iterate through the list items
+    for i, item in ipairs(element.content) do
+      -- Check if the item contains a paragraph block
+      if item.tag == "Plain" then
+        -- Remove the paragraph block and keep its inline contents
+        element.content[i] = pandoc.Span(item.content)
+      end
+    end
   end
-  return el
+  return element
 end
+
+return {
+  { Inline = removePTagsInLiBlocks },
+  { Block = removePTagsInLiBlocks }
+}
