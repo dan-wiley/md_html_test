0003f3c3ca4601abcdc18e4cbf8654b2bb85ed42
dan-wiley <119693510+dan-wiley@users.noreply.github.com>
2023-05-20 12:07:38 -0700

    Update and rename remove-p-tags.lua to remove-p-tags.py

---
+++ remove-p-tags.py
@@ -0,0 +1,23 @@
+import panflute as pf
+
+def remove_p_tags_within_li(elem):
+    if isinstance(elem, (pf.OrderedList, pf.BulletList)):
+        elem.content = [remove_p_tags_within_li(item) if isinstance(item, pf.ListItem) else item for item in elem.content]
+    elif isinstance(elem, pf.ListItem):
+        elem.content = [remove_p_tags_within_li(subelem) if isinstance(subelem, pf.Para) else subelem for subelem in elem.content]
+        elem.content = [subelem for subelem in elem.content if not (isinstance(subelem, pf.Para) and has_nested_p_tags(subelem))]
+    return elem
+
+def has_nested_p_tags(elem):
+    if isinstance(elem, pf.Para):
+        return True
+    elif isinstance(elem, (pf.Div, pf.Span, pf.Quoted, pf.Math)):
+        return any(has_nested_p_tags(subelem) for subelem in elem.content)
+    elif isinstance(elem, pf.Code):
+        return has_nested_p_tags(elem.text)
+    elif isinstance(elem, (list, tuple)):
+        return any(has_nested_p_tags(sublist) for sublist in elem)
+    return False
+
+if __name__ == "__main__":
+    pf.run_filter(remove_p_tags_within_li)
