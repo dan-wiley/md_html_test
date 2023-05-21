51c3a6f72d4b08df9b8abf7c42eacab5f10cbd01
dan-wiley <119693510+dan-wiley@users.noreply.github.com>
2023-05-20 22:11:24 -0700

    Update wrap_code_tags_with_pre_filter.py

--- python_scripts/wrap_code_tags_with_pre_filter.py
+++ python_scripts/wrap_code_tags_with_pre_filter.py
@@ -1,10 +1,10 @@
 import panflute as pf
 
 def wrap_code_blocks(elem, doc):
-    if isinstance(elem, pf.Code) and (not isinstance(elem.parent, pf.RawBlock) or elem.parent.format != "html" or elem.parent.text.strip().lower() != "<pre>"):
-        pre_tag = pf.RawBlock("<pre>")
-        pre_tag.content.append(pf.Plain(elem))
-        pre_tag.content.append(pf.RawBlock("</pre>"))
+    if isinstance(elem, pf.Code) and not any(isinstance(parent, pf.CodeBlock) for parent in elem.ancestors()):
+        pre_tag = pf.RawBlock("<pre>", format="html")
+        pre_tag.text = elem.text
+        pre_tag.text += "</pre>"
         return pre_tag
 
 if __name__ == "__main__":
