3db2d32b0757b8d79553e76670d10a965bea3c7f
dan-wiley <119693510+dan-wiley@users.noreply.github.com>
2023-05-23 08:24:40 -0700

    Update clean_code_tag.py

--- python_scripts/clean_code_tag.py
+++ python_scripts/clean_code_tag.py
@@ -1,22 +1,17 @@
-from bs4 import BeautifulSoup
-import panflute as pf
+#!/usr/bin/env python3
+from panflute import Code, RawInline, run_filter
 
-def strip_html_tags(elem, doc):
-    if isinstance(elem, pf.RawInline) and elem.format == 'html':
-        print("Processing RawInline element:", elem)
-        soup = BeautifulSoup(elem.text, 'html.parser')
-        code_tags = soup.find_all('code')
-        print("Number of code tags found:", len(code_tags))
-        for tag in code_tags:
-            # Replace <code> tag with its plain text content
-            tag.string.replace_with(tag.text)
-        elem.text = str(soup)
-        print("Modified element:", elem)
+
+def strip_code_tags(elem):
+    if isinstance(elem, Code):
+        # Remove all child elements within <code>
+        elem.content = [RawInline(elem.text)]
     return elem
 
 
 def main(doc=None):
-    return pf.run_filter(strip_html_tags, doc=doc)
+    return run_filter(strip_code_tags, doc=doc)
+
 
 if __name__ == "__main__":
     main()
