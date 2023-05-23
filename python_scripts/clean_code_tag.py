b5f5386a9f4507916a2cf10d2b5bebab02d0491e
dan-wiley <119693510+dan-wiley@users.noreply.github.com>
2023-05-23 09:34:36 -0700

    Update clean_code_tag.py

--- python_scripts/clean_code_tag.py
+++ python_scripts/clean_code_tag.py
@@ -6,13 +6,10 @@ def strip_code_tags(elem, doc):
     if isinstance(elem, (Code, CodeBlock)):
         # Parse the code text with BeautifulSoup
         soup = BeautifulSoup(elem.text, 'html.parser')
-        code_tags = soup.find_all('code')
+        code_tags = soup.find_all()
         for code_tag in code_tags:
             # Remove all child elements within each code tag
-            for child in code_tag.children:
-                child.extract()
-            # Remove empty <code> tags
-            code_tag.decompose()
+            code_tag.unwrap()
         # Update the code text with the modified content
         elem.text = str(soup)
         return elem
