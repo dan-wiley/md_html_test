360a3924fd6a9915da46689a415ac0e255810733
dan-wiley <119693510+dan-wiley@users.noreply.github.com>
2023-05-23 08:13:30 -0700

    Update clean_code_tag.py

--- python_scripts/clean_code_tag.py
+++ python_scripts/clean_code_tag.py
@@ -3,13 +3,17 @@ import panflute as pf
 
 def strip_html_tags(elem, doc):
     if isinstance(elem, pf.RawInline) and elem.format == 'html':
+        print("Processing RawInline element:", elem)
         soup = BeautifulSoup(elem.text, 'html.parser')
         code_tags = soup.find_all('code')
+        print("Number of code tags found:", len(code_tags))
         for tag in code_tags:
             # Replace <code> tag with its plain text content
             tag.string.replace_with(tag.text)
         elem.text = str(soup)
+        print("Modified element:", elem)
     return elem
+
 
 def main(doc=None):
     return pf.run_filter(strip_html_tags, doc=doc)
