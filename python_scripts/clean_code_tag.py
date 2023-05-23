a440091fd2e0aa13b7ecdcc9294e446f5649e0ef
dan-wiley <119693510+dan-wiley@users.noreply.github.com>
2023-05-23 09:42:56 -0700

    Update clean_code_tag.py

--- python_scripts/clean_code_tag.py
+++ python_scripts/clean_code_tag.py
@@ -1,23 +1,17 @@
 from panflute import run_filter, Code, CodeBlock
 from bs4 import BeautifulSoup
-import os
 
 
 def strip_code_tags(elem, doc):
     if isinstance(elem, (Code, CodeBlock)):
-        # Check if elem.text is a valid file
-        if os.path.isfile(elem.text):
-            # Read the file contents
-            with open(elem.text, 'r') as file:
-                file_contents = file.read()
-            # Parse the file contents with BeautifulSoup
-            soup = BeautifulSoup(file_contents, 'html.parser')
-            code_tags = soup.find_all()
-            for code_tag in code_tags:
-                # Remove all child elements within each code tag
-                code_tag.unwrap()
-            # Update the code text with the modified content
-            elem.text = str(soup)
+        # Parse the code text with BeautifulSoup
+        soup = BeautifulSoup(elem.text, 'html.parser')
+        code_tags = soup.find_all('code')
+        for code_tag in code_tags:
+            # Remove all child tags within each code tag
+            code_tag.clear()
+        # Update the code text with the modified content
+        elem.text = str(soup)
         return elem
 
 
