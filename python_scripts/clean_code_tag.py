7601ae767f186e2d84b04808e39b1202b2bdb67c
dan-wiley <119693510+dan-wiley@users.noreply.github.com>
2023-05-23 08:37:09 -0700

    Update clean_code_tag.py

--- python_scripts/clean_code_tag.py
+++ python_scripts/clean_code_tag.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-from bs4 import BeautifulSoup
+import re
 from panflute import run_filter, RawInline
 
 
 def strip_code_tags(elem, doc):
     if elem.tag == 'code':
-        soup = BeautifulSoup(str(elem), 'html.parser')
-        text = soup.get_text()
-        return RawInline(text)
+        # Remove all child elements and keep the text
+        elem.content = [RawInline(elem.text)]
     return None
 
 
