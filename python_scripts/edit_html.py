060a445ff850d8f831ab001ac05942f66b6d4f18
dan-wiley <119693510+dan-wiley@users.noreply.github.com>
2023-05-23 10:04:01 -0700

    Update edit_html.py

--- python_scripts/edit_html.py
+++ python_scripts/edit_html.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import sys
 
-def modify_code_tags(html_file, tag):
+def modify_code_tags(html_file):
     """
     Wrap <code> tags in <pre> tags if the immediate parent is not <pre>.
     Adds the 
@@ -30,7 +30,7 @@ def modify_code_tags(html_file, tag):
         html_content = f.read()
 
     soup = BeautifulSoup(html_content, 'html.parser')
-    code_blocks = soup.find_all(tag)
+    code_blocks = soup.find_all('code')
     for code_block in code_blocks:
         pre_tag = code_block.find_parent('pre')
         if pre_tag is None:
@@ -45,8 +45,8 @@ def modify_code_tags(html_file, tag):
                     classes.append(class_map[lang])
         pre_tag['class'] = ' '.join(classes)
 
-        # Remove tags inside <code> and replace with text
-        code_block.unwrap()
+        for tag in code_block.find_all():
+            tag.unwrap()
 
     with open(html_file, 'w') as f:
         f.write(str(soup))
@@ -58,4 +58,4 @@ if __name__ == "__main__":
 
     html_file = sys.argv[1]
     tag = 'code'
-    modify_code_tags(html_file, tag)
+    modify_code_tags(html_file)
